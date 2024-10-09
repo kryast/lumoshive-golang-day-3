c34ea795652aeb31635619bb1469919cb14d13e3
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-09 11:09:44 +0700

    latihan 3

--- latihan/script.js
+++ latihan/script.js
@@ -44,3 +44,29 @@ let daftarBarang = [produk1,produk2,produk3];
 console.log(daftarBarang[1].name);
 console.log(daftarBarang[0].price);
 console.log(daftarBarang[2].stock);
+
+// buatkan text judul di html menggunakan h1 lalu lakukan manipulasi h1 menggunakan javascript,
+// dengan ketentuan berikut ini :
+// saat pertama kali diakses web muncul teks "Lumoshive-Academy"
+// lalu saat tombol ditekan teks berubah menjadi "programmer handal"
+// lalu saat tombol ditekan lagi teks berubah lagi menjadi "Aku pasti bisa menjadi programmer"
+
+let judul = document.getElementById("judul");
+let tombol = document.getElementById("tombol");
+let klik = 0;
+
+tombol.addEventListener('click', function(){
+    klik++;
+    switch (klik) {
+        case 1:
+            judul.innerHTML = "programmer handal";
+            break;
+        case 2:
+            judul.innerHTML = 'Aku pasti bisa menjadi programmer';
+            break;
+    }
+    
+});
+
+
+
