b8ea341745d91b3717b22bb036ccccaf72287161
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-09 12:25:34 +0700

    latihan 4

---
+++ latihan-2/script.js
@@ -0,0 +1,19 @@
+document.getElementById("tombol").addEventListener("click", function(){
+let list = document.getElementById("list");
+let text = document.createElement("li");
+let hapus = document.createElement("button");
+
+let input = document.getElementById("input").value;
+text.innerHTML = input;
+
+hapus.id = "dihapus";
+hapus.innerHTML = "X";
+
+list.appendChild(text);
+text.appendChild(hapus);
+
+hapus.addEventListener("click", function(){
+    list.removeChild(text);
+
+});
+});
